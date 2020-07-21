fa4a2ab136916f4ee57d600b5ee21abe5f0c980a
bruno pessoa <brunopessoa.melo@gmail.com>
2020-07-21 15:35:12 -0300

    ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CasesComponent } from './cases/cases.component';
+import { CasesDetailsComponent } from './cases-details/cases-details.component';
+import { CasesStatComponent } from './cases-stat/cases-stat.component';
+import { AddCasesComponent } from './add-cases/add-cases.component';
+import { EditCasesComponent } from './edit-cases/edit-cases.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'cases',
+    component: CasesComponent,
+    data: {title: 'Lista de Casos'}
+  },
+
+  {
+    path: 'cases-details/:id',
+    component: CasesDetailsComponent,
+    data: {title: 'Detalhes de Casa'}
+  },
+
+  {
+    path: 'cases-stat',
+    component: CasesStatComponent,
+    data: {title: 'Casos Estaticos'}
+  },
+
+  {
+    path: 'add-cases',
+    component: AddCasesComponent,
+    data: {title: 'Casos Adicionados'}
+  },
+
+  {
+    path: 'edit-cases/:id',
+    component: EditCasesComponent,
+    data: {title: 'Editar Casos'}
+  },
+
+  {
+    path: '',
+    redirectTo: '/cases',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
